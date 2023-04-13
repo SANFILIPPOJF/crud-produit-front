cf06aa0e84e66b163f3e91cbd55b4a68ddf46f8f
SANFILIPPOJF <jean-francois.sanfilippo@sncf.fr>
2023-04-13 14:34:58 +0200

    feat: titre

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
+import { Titre } from './components/titre';
 
 function App() {
+  const [page, setPage] = useState("accueil");
   return (
     <div className="App">
+      <Titre></Titre>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
---
+++ src/components/titre.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export function Titre() {
+    return(
+        <div>
+        <h1 className= "titre" style= {{fontSize: "5rem",color:"teal"}}>Produits</h1>
+        </div>
+    )
+};
